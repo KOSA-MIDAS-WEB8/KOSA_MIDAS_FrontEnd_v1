2eb266c75c650d7fa2842ab69b0a37aa43c6026c
jingeon27 <jingeon27@gmail.com>
2022-11-03 13:57:02 +0900

    ADD :: Loadingpage 추가

--- src/status/loading/index.tsx
+++ src/status/loading/index.tsx
@@ -7,12 +7,24 @@ const Loading: React.FC = () => {
   );
 };
 export default Loading;
-const Spinner = styled.div``;
-const Turn = keyframes`
+const turn = keyframes`
   0% {
      transform: rotate(0deg);
   }
   100% {
     transform: rotate(360deg);
   }
+`;
+const Spinner = styled.div`
+  position: relative;
+  left: 200px;
+  top: 200px;
+  width: 300px;
+  height: 300px;
+  border: 20px solid ${(props) => props.theme.colors.white};
+  border-bottom-color: ${(props) => props.theme.colors.purple};
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: ${turn} 1s linear infinite;
 `;
