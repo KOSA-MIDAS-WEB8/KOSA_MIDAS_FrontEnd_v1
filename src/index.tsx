b9bc18c802ba583758e2f677c7d33d2110559a72
jingeon27 <jingeon27@gmail.com>
2022-11-04 02:28:09 +0900

    Hotfix :: error수정

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
