88ca1ba258f3dc2bc0c79918944f4c5a7a1bfe62
jingeon27 <jingeon27@gmail.com>
2022-11-03 14:31:32 +0900

    FIX :: 폴더명 수정

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,12 @@
 export const theme = {
   font: { en: "Noto Sans", kr: "Noto Sans KR" },
-  colos: {
+  colors: {
     purple: "#755ADB",
     blue: "#2F55DD",
     skyBlue: "#6C86E7",
     white: "#FFFFFF",
     gray: "#D9D9D9",
     black: "#000000",
+    error: "#C73E1D",
   },
 };
