ae6b24509e8a138c34870fc059946e3ccad8e595
jingeon27 <jingeon27@gmail.com>
2022-11-03 12:26:38 +0900

    ADD ☀ :: notosans, notosanskr 추가

--- src/status/loading/index.tsx
+++ src/status/loading/index.tsx
@@ -1,8 +1,18 @@
+import styled, { keyframes } from "styled-components";
 const Error: React.FC = () => {
   return (
     <>
-      <div>error입니다.</div>
+      <Spinner />
     </>
   );
 };
 export default Error;
+const Spinner = styled.div``;
+const Turn = keyframes`
+  0% {
+     transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
